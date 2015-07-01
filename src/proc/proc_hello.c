c04f9d54d2cd75b9d6de83e1c2558494398a6bd8
yinqingwang <yinqingwang@gmail.com>
2015-07-01 10:51:28 +0800

    add /proc/hello file system

--- src/proc/proc_hello.c
+++ src/proc/proc_hello.c
@@ -8,7 +8,12 @@
 #include <linux/moduleparam.h> // Needed for module_param
 
 /* Necessary because we use the proc fs */
+#include <linux/fs.h>
+#include <linux/init.h>
 #include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/utsname.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("yinqingwang@gmail.com");
@@ -108,6 +113,7 @@ static int procfile_read(char *buffer,
 }
 #endif
 
+#if 0
 struct proc_dir_entry *hello_proc_dir = NULL;
 struct proc_dir_entry *hello_proc_file = NULL;
 
@@ -144,9 +150,55 @@ static void exit_hello_module(void)
 	printk(KERN_ALERT "Hello Proc module: Goodbye! module exit.\n");
 }
 
+#endif
+
+
+static int hello_proc_show(struct seq_file *m, void *v)
+{
+    seq_printf(m,"Hello module!\n");
+    return 0;
+}
+
+static int hello_proc_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, hello_proc_show, NULL);
+}
+
+static const struct file_operations hello_proc_fops = {
+    .open       = hello_proc_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+};
+
+
+static struct proc_dir_entry *hello_proc_file = NULL;
+
+/* Initialize the module - register the proc file */
+static int init_hello_module(void)
+{
+	printk(KERN_ALERT "Hello Proc module: Hello, module is init! \n");
+
+    hello_proc_file = proc_create("hello", 0, NULL, &hello_proc_fops);
+	
+    if( hello_proc_file == NULL){
+        printk("Cann't creat 'hello' in /proc. \n");
+        return -1; 
+    }
+	
+    //A non 0 return means init_module failed; module can't be loaded.
+    return 0;
+}
+
+/* Remove the module */
+static void exit_hello_module(void)
+{
+    proc_remove( hello_proc_file);
+    printk(KERN_ALERT "Hello Proc module: Goodbye! module exit.\n");
+}
+
 
 //here is the module init/exit
 module_init(init_hello_module);
 module_exit(exit_hello_module);
-
 
